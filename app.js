34b700d2f9e30b768593a6f816cf1bf598527b0b
Blugamma <blugammadesign@gmail.com>
2019-02-22 13:01:04 +0000

    initial Upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+const fs = require('fs');
+app.use(express.static('public'));
+
+
+app.get('/',function(req,res) {
+    res.sendFile('C:/Users/Ollie/Desktop/FYP/ArTest/index.html');
+  });
+
+// we will pass our 'app' to 'https' server
+ app.listen(8080);
