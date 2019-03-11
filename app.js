b245f507dc90f073d233f6a9c77f2a8ee429deb4
Blugamma <blugammadesign@gmail.com>
2019-03-11 19:53:24 +0000

    Fixed blurry canvas and started Image upload code

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
+const multer  = require('multer');
 const https = require('https');
 const fs = require('fs');
 const port = 8080;
 app.use(express.static('public'));
+app.use(express.static('uploads'));
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ 
+  storage: storage,
+  limits: {
+    fieldSize: 8 * 1024 * 1024,
+    fieldNameSize: 8 * 1024 * 1024,
+  },
+ });
 
 app.get('/',function(req,res) {
     res.sendFile('index.html');
@@ -13,4 +30,10 @@ app.get('/',function(req,res) {
 // we will pass our 'app' to 'https' server
 app.listen(port, () => {
   console.log(`App is listening to ${port}`);
+});
+
+app.post('/imageUpload', upload.single('image'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log(req.file.originalname);
 });
